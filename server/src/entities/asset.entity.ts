1b7a2cf84571683ff6d33ebe41f3dff334a6230f
mertalev <101130780+mertalev@users.noreply.github.com>
2024-04-27 20:03:11 -0400

    add index to entity

--- server/src/entities/asset.entity.ts
+++ server/src/entities/asset.entity.ts
@@ -169,6 +169,7 @@ export class AssetEntity {
   @OneToOne(() => AssetJobStatusEntity, (jobStatus) => jobStatus.asset, { nullable: true })
   jobStatus?: AssetJobStatusEntity;
 
+  @Index('IDX_assets_duplicateId')
   @Column({ type: 'uuid', nullable: true })
   duplicateId!: string | null;
 }
